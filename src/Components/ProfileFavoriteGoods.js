5d038c652d9e95c98a17f19b683812691596fdeb
Brian Sahota <bpsahota@gmail.com>
2021-03-02 19:00:31 -0500

    stuck on delete function

--- src/Components/ProfileFavoriteGoods.js
+++ src/Components/ProfileFavoriteGoods.js
@@ -1,15 +1,26 @@
 import React from 'react'
 
 
-const ProfileFavoriteGoods = ({ item }) => {
+const ProfileFavoriteGoods = ({ item, onDeleteFavItem }) => {
+    const { id, name, image, likes } = item
+
+    const handleRemoveFavoriteItem = () => {
+        fetch(`http://localhost:3000/api/v1/fav_goods/${id}`,{
+            method: "DELETE",
+        })
+        .then((r) => r.json())
+        .then(() => {
+            onDeleteFavItem(item)
+        })
+    }
 
     return (
         <div>
-            <h3>{item.good.name}</h3>
-            <img src={item.good.image} alt={item.good.name} />
-            <button>{item.good.likes}</button>
+            <h3>{name}</h3>
+            <img src={image} alt={name} />
+            <button>{likes}</button>
             <button>Add a little note to self</button>
-            <button>Remove from favs</button>
+            <button onClick={handleRemoveFavoriteItem} >Remove from favs</button>
         </div>
     )
 }
--- src/Pages/Profile.js
+++ src/Pages/Profile.js
@@ -1,10 +1,13 @@
 import React from 'react' 
 import { useSelector } from 'react-redux' 
+import { useDispatch } from 'react-redux'
+import { addFavGoods } from '../Components/redux/fav_good'
 //Components
 import ProfileFavoriteGoods from '../Components/ProfileFavoriteGoods'
 import ProfileFavoritesRecipes from '../Components/ProfileFavoritesRecipes'
 
 const Profile = () => {
+    const dispatch = useDispatch()
     // from redux
     const profileFavoriteGoods = useSelector((state) => state.favGoods.goods)
     const myFavoriteRecipes = useSelector((state) => state.favRecipes.myFavRecipes)
@@ -12,9 +15,14 @@ const Profile = () => {
     console.log(profileFavoriteGoods)
     console.log(myFavoriteRecipes)
 
+    const onDeleteFavItem = (itemToDelete) => {
+        const updatedFavGoodArray = profileFavoriteGoods.filter((otherGoods) => otherGoods.id !==itemToDelete.id )
+        dispatch(addFavGoods(updatedFavGoodArray))
+    }
+    
     // our iterations from redux
     const allMyFavoriteItems = profileFavoriteGoods.map((item) => (
-        <ProfileFavoriteGoods key={item.id} item={item} />
+        <ProfileFavoriteGoods key={item.id} item={item.good} onDeleteFavItem={onDeleteFavItem} />
     ))
     const allMyFavoriteRecipes = myFavoriteRecipes.map((rec) => (
         <ProfileFavoritesRecipes key={rec.id} recipes={rec.recipe}/>
