1ac801e95811ac6e5ac202b301870b1da7b453e8
Brian Sahota <bpsahota@gmail.com>
2021-03-06 15:35:26 -0500

    maps working

--- src/Pages/Stores.js
+++ src/Pages/Stores.js
@@ -1,10 +1,24 @@
 import React from 'react' 
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 
 const Stores = () => {
     return (
-        <div className="store" >
-            <h2>hopefully we can get like a list of stores to be routed to..we'll figure this out later</h2>
-        </div>
+        <main>
+            <MapContainer center={[40.7008739, -73.9875141]} 
+            zoom={13} 
+            scrollWheelZoom={true} 
+            style={{ height: "600px"}}>
+                <TileLayer
+                    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                <Marker position={[40.7008739, -73.9875141]}>
+                    <Popup>
+                    A pretty CSS3 popup. <br /> Easily customizable.
+                    </Popup>
+                </Marker>
+                </MapContainer>
+        </main>
     )
 }
 
