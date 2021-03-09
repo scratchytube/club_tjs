769f6461ace35dcf76467fe6c1d9047c5e031997
Brian Sahota <bpsahota@gmail.com>
2021-03-09 09:05:59 -0500

    i like the carousel

--- src/Components/HomeCarousel.js
+++ src/Components/HomeCarousel.js
@@ -2,38 +2,35 @@ import React from 'react'
 import Carousel from 'react-bootstrap/Carousel'
 // images
 import TJFlowers from '../images/tjflowers.jpg'
-import TJInside from '../images/tj_inside.jpg'
-import TJGroceries from '../images/tjgroceries.jpg'
-import TJArt from '../images/tj_art.jpg'
+import TJCart from '../images/tjCart.jpg'
+import TJOutside from '../images/outsidetjs.jpg'
+
+
 
 
 const HomeCarousel = () => {
     return (
 
 <Carousel >
-    <Carousel.Item interval="1000">
+
+    <Carousel.Item interval="12341000">
         <img 
         className="d-block w-100"
         src={TJFlowers} alt="flowers" />
     </Carousel.Item>
 
-    <Carousel.Item interval="1000">
+    <Carousel.Item interval="12341000">
         <img 
         className="d-block w-100"
-        src={TJInside} alt="inside" />
+        src={TJCart} alt="shopping cart" />
     </Carousel.Item>
 
-    <Carousel.Item interval="1000">
+    <Carousel.Item interval="12341000">
         <img 
         className="d-block w-100"
-        src={TJGroceries} alt="groceries" />
+        src={TJOutside} alt="groceries" />
     </Carousel.Item>
 
-    <Carousel.Item interval="1000">
-        <img 
-        className="d-block w-100"
-        src={TJArt} alt="flowers" />
-    </Carousel.Item>
 </Carousel>
     )
 }
